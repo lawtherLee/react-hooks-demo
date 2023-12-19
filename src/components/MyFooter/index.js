9b81927d7a5a41073636e667bc4d97949b5f6683
尚泽 <108440505+lawtherLee@users.noreply.github.com>
2023-12-19 23:29:31 +0800

    结算总价，商品总数

--- src/components/MyFooter/index.js
+++ src/components/MyFooter/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import "./index.scss";
 export default function MyFooter({ list, updateAllState }) {
   const isCheckAll = list.every((item) => item.goods_state);
+
+  const total = list
+    .filter((item) => item.goods_state)
+    .reduce((sum, item) => {
+      return (sum += item.goods_count);
+    }, 0);
+
+  const totalPrice = list
+    .filter((item) => item.goods_state)
+    .reduce((sum, item) => {
+      return (sum += item.goods_count * item.goods_price);
+    }, 0);
+
   return (
     <div className="my-footer">
       <div className="custom-control custom-checkbox">
@@ -18,10 +31,10 @@ export default function MyFooter({ list, updateAllState }) {
       </div>
       <div>
         <span>合计:</span>
-        <span className="price">¥ 100</span>
+        <span className="price">¥ {totalPrice}</span>
       </div>
       <button type="button" className="footer-btn btn btn-primary">
-        结算 (0)
+        结算 ({total})
       </button>
     </div>
   );
